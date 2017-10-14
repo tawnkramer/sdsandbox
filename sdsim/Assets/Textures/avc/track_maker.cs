f31c3a84d421ab87b8e3ae85d745e8af558c339c
Tawn Kramer <tawnkramer@gmail.com>
2017-10-13 21:00:53 -0700

    creating avc track

---
+++ sdsim/Assets/Textures/avc/track_maker.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class track_maker : MonoBehaviour {
+
+	public GameObject hayPrefab;
+
+	public int numStraightSeg = 0;
+	public float offset = 1.0f;
+	public float curveYDeg = 5.0f;
+	public int numCurve = 0;
+
+	public Transform startTm;
+	public Transform root;
+	public bool Rotate180Y = false;
+
+	#if UNITY_EDITOR
+	[MenuItem("CONTEXT/track_maker/CreateTrackInEditor")]
+    static void CreateTrackInEditor(MenuCommand command)
+    {
+        track_maker go = (track_maker)command.context;
+        go.CreateTrack();
+    }
+	#endif
+
+	void Start () 
+	{
+		CreateTrack();
+	}
+
+	// Use this for initialization
+	public void CreateTrack() 
+	{		
+		for(int i = 0; i < numStraightSeg; i++)
+		{
+			GameObject go = GameObject.Instantiate(hayPrefab) as GameObject;
+
+			Undo.RegisterCreatedObjectUndo(go, "track object");
+
+			go.transform.parent = root.transform;
+
+			go.transform.localScale = hayPrefab.transform.localScale;
+			go.transform.rotation = startTm.rotation;
+			go.transform.position = startTm.position;
+
+			if(Rotate180Y)
+				go.transform.Rotate(0, 180, 0);
+
+			startTm.Translate(offset + hayPrefab.transform.localScale.x, 0, 0);	
+		}
+
+		for(int i = 0; i < numCurve; i++)
+		{
+			GameObject go = GameObject.Instantiate(hayPrefab) as GameObject;
+
+			Undo.RegisterCreatedObjectUndo(go, "track object");
+
+			go.transform.parent = root.transform;
+
+			go.transform.localScale = hayPrefab.transform.localScale;
+			go.transform.rotation = startTm.rotation;
+			go.transform.position = startTm.position;
+
+			if(Rotate180Y)
+				go.transform.Rotate(0, 180, 0);
+
+			startTm.Rotate(0, curveYDeg, 0);	
+			startTm.Translate(offset + hayPrefab.transform.localScale.x, 0, 0);	
+		}
+
+		for(int i = 0; i < numStraightSeg; i++)
+		{
+			GameObject go = GameObject.Instantiate(hayPrefab) as GameObject;
+
+			Undo.RegisterCreatedObjectUndo(go, "track object");
+
+			go.transform.parent = root.transform;
+
+			go.transform.localScale = hayPrefab.transform.localScale;
+			go.transform.rotation = startTm.rotation;
+			go.transform.position = startTm.position;
+
+			if(Rotate180Y)
+				go.transform.Rotate(0, 180, 0);
+
+			startTm.Translate(offset + hayPrefab.transform.localScale.x, 0, 0);	
+		}
+	}	
+	
+}
