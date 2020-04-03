cd000cbdc5a4ba3fcf8c0a063f174d47240478c7
Tawn Kramer <tawnkramer@gmail.com>
2020-04-03 15:58:51 -0700

    improved test client

--- src/test_client.py
+++ src/test_client.py
@@ -2,23 +2,73 @@ import os
 import random
 import json
 import time
+from io import BytesIO
+import base64
 
+from PIL import Image
+import numpy as np
 from gym_donkeycar.core.sim_client import SDClient
+
 
 ###########################################
 
+class SimpleClient(SDClient):
+
+    def __init__(self, address, poll_socket_sleep_time=0.01):
+        super().__init__(*address, poll_socket_sleep_time=poll_socket_sleep_time)
+        self.last_image = None
+        self.car_loaded = False
+
+    def on_msg_recv(self, json_packet):
+
+        if json_packet['msg_type'] == "car_loaded":
+            self.car_loaded = True
+        
+        if json_packet['msg_type'] == "telemetry":
+            imgString = json_packet["image"]
+            image = Image.open(BytesIO(base64.b64decode(imgString)))
+            self.last_image = np.asarray(image)
+
+            #don't have to, but to clean up the print, delete the image string.
+            del json_packet["image"]
+
+        print("got:", json_packet)
+
+
+    def send_controls(self, steering, throttle):
+        p = { "msg_type" : "control",
+                "steering" : steering.__str__(),
+                "throttle" : throttle.__str__(),
+                "brake" : "0.0" }
+        msg = json.dumps(p)
+        self.send(msg)
+
+        #this sleep lets the SDClient thread poll our message and send it out.
+        time.sleep(self.poll_socket_sleep_sec)
+
+    def update(self):
+        # just random steering now
+        st = random.random() * 2.0 - 1.0
+        th = 0.3
+        self.send_controls(st, th)
+
+
+
+###########################################
+## Make some clients and have them connect with the simulator
+
 def test_clients():
     # test params
-    host_ip = "127.0.0.1" # "trainmydonkey.com" for virtual racing server
+    host = "127.0.0.1" # "trainmydonkey.com" for virtual racing server
     port = 9091
-    num_clients = 2
+    num_clients = 1
     clients = []
-    time_to_drive = 40.0
+    time_to_drive = 10.0
 
 
     # Start Clients
     for _ in range(0, num_clients):
-        c = SDClient(host_ip, port)
+        c = SimpleClient(address=(host, port))
         clients.append(c)
 
     time.sleep(1)
@@ -28,16 +78,16 @@ def test_clients():
     clients[0].send(msg)
 
     # Wait briefly for the scene to load.
-    time.sleep(1.0)
+    loaded = False
+    while(not loaded):
+        time.sleep(1.0)
+        for c in clients:
+            loaded = c.car_loaded           
+        
 
     # Car config
     msg = '{ "msg_type" : "car_config", "body_style" : "donkey", "body_r" : "255", "body_g" : "0", "body_b" : "255", "car_name" : "Tawn", "font_size" : "100" }'
     clients[0].send(msg)
-    time.sleep(1)
-
-    msg = '{ "msg_type" : "car_config", "body_style" : "car01", "body_r" : "0", "body_g" : "0", "body_b" : "255", "car_name" : "Doug" }'
-    if num_clients > 1:
-        clients[1].send(msg)
     time.sleep(1)
 
 
@@ -46,14 +96,7 @@ def test_clients():
     do_drive = True
     while time.time() - start < time_to_drive and do_drive:
         for c in clients:
-            st = random.random() * 2.0 - 1.0
-            th = 0.3
-            p = { "msg_type" : "control",
-                "steering" : st.__str__(),
-                "throttle" : th.__str__(),
-                "brake" : "0.0" }
-            msg = json.dumps(p)
-            c.send(msg)
+            c.update()
             if c.aborted:
                 print("Client socket problem, stopping driving.")
                 do_drive = False
