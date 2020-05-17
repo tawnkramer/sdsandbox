be77d18a25bcbf007ab64e886e055e37432aa745
Tawn Kramer <tawnkramer@gmail.com>
2020-05-16 17:47:49 -0700

    handle spawn spacing better in mountain

--- sdsim/Assets/Scripts/CarSpawner.cs
+++ sdsim/Assets/Scripts/CarSpawner.cs
@@ -12,9 +12,9 @@ public class CarSpawner : MonoBehaviour {
 
 	public delegate void OnNewCar(GameObject carObj);
 	public OnNewCar OnNewCarCB;	
+    public float m_CarLeftRightSpace = 4.5f;
 
-
-	public Camera splitScreenCamLeft;
+    public Camera splitScreenCamLeft;
 	public Camera splitScreenCamRight;
 	public GameObject splitScreenPanel;
 
@@ -188,10 +188,10 @@ public class CarSpawner : MonoBehaviour {
 
         //just stack more cars after the second. Not pretty.
         int iRow = iCar / 2;
-        offset = Vector3.forward * (-5f * iRow);
+        offset = startTm.forward * (-5f * iRow);
 
         if ((iCar + 1) % 2 == 0)
-            offset += Vector3.left * 4.5f;
+            offset += startTm.right * -1 * m_CarLeftRightSpace;
 
         Vector3 startPos = startTm.position + offset;
 
