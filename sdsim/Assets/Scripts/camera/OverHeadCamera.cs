99c5c861c46b991f4bbfa9b8eca2224610a05346
Maxime Ellerbach <maxime@ellerbach.net>
2021-07-20 22:44:44 +0200

    overhead camera now works

--- sdsim/Assets/Scripts/camera/OverHeadCamera.cs
+++ sdsim/Assets/Scripts/camera/OverHeadCamera.cs
@@ -9,7 +9,10 @@ public class OverHeadCamera : MonoBehaviour
     public float height = 10;
     Camera cam;
 
-    public void Init ()
+    float previousHeight = 0;
+    float previousWidth = 0;
+
+    public void Init()
     {
         if (pathManager == null) { return; }
 
@@ -25,29 +28,62 @@ public class OverHeadCamera : MonoBehaviour
         float zmin = float.MaxValue;
         float zmax = float.MinValue;
 
+        Vector3 vxmin = new Vector3();
+        Vector3 vzmin = new Vector3();
+
         foreach (PathNode centerNode in pathManager.carPath.centerNodes)
         {
             Vector3 pos = centerNode.pos;
             points.Add(pos);
             if (pos.x < xmin)
+            {
                 xmin = pos.x;
+                vxmin = pos;
+            }
             if (pos.z < zmin)
+            {
                 zmin = pos.z;
+                vzmin = pos;
+            }
             if (pos.x > xmax)
+            {
                 xmax = pos.x;
+            }
             if (pos.z > zmax)
+            {
                 zmax = pos.z;
+            }
         }
 
-        // place the camera in the center of the path
+        // place the camera in the center of the circuit
         Vector3 center = new Vector3((xmin + xmax) / 2.0f, 0, (zmin + zmax) / 2.0f);
         transform.position = center + Vector3.up * height;
+        transform.LookAt(center, Vector3.up);
 
-        float xSize = Mathf.Abs(xmax - xmin);
-        float zSize = Mathf.Abs(zmax - zmin);
-        float camSize = Mathf.Max(xSize, zSize);
-
-        cam.orthographicSize = (camSize / 2) + margin;
         // try to best fit the camera to the screen
+        Vector3 vpxmin = cam.WorldToViewportPoint(vxmin);
+        Vector3 vpzmin = cam.WorldToViewportPoint(vzmin);
+
+        float xdistanceToBorder = vpxmin.x;
+        float zdistanceToBorder = vpzmin.y;
+
+        float zoomValue = 1 - Mathf.Min(xdistanceToBorder, zdistanceToBorder) * 2;
+        cam.orthographicSize = cam.orthographicSize * zoomValue + margin;
     }
+
+    public void Update()
+    {
+        float currentHeight = Display.main.renderingHeight;
+        float currentWidth = Display.main.renderingWidth;
+
+        // if the resolution of the window changed, re-Init the camera
+        if (currentHeight != previousHeight || currentWidth != previousWidth)
+        {
+            previousHeight = currentHeight;
+            previousWidth = currentWidth;
+
+            Init();
+        }
+    }
+
 }
