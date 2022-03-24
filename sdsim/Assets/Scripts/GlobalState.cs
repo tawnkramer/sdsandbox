392d6efef0a1bfcd59ad3b4527d9d70629445a25
Maxime Ellerbach <maxime@ellerbach.net>
2022-03-24 23:06:21 +0100

    version bump

--- sdsim/Assets/Scripts/GlobalState.cs
+++ sdsim/Assets/Scripts/GlobalState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class GlobalState
 {
-    public static string version = "v21.12.11";
+    public static string version = "v22.03.24";
     public static string host = "0.0.0.0";
     public static int port = 9091;
     public static int portPrivateAPI = 9092;
