ec04c400f045222375ec82e649bb051fb17aad41
Maxime Ellerbach <maxime@ellerbach.net>
2025-10-06 12:03:25 +0200

    Version bump for Unity vulnerability (#91)
    * ice fix
    
    * removing pandemic edition
    
    * version bump

--- sdsim/Assets/Scripts/GlobalState.cs
+++ sdsim/Assets/Scripts/GlobalState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class GlobalState
 {
-    public static string version = "v22.05.30";
+    public static string version = "v25.06.10";
     public static string host = "0.0.0.0";
     public static int port = 9091;
     public static int portPrivateAPI = 9092;
